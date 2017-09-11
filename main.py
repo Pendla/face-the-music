89d8af966fba863e90f524a0aa918862cfe70740
Simon Petersson <peterssonaren@gmail.com>
2017-09-10 20:44:24 -0400

    Add extraction of image paths from face folder

--- main.py
+++ main.py
@@ -1,20 +1,56 @@
+import os
 import sys
+import collections
+
 import face_recognition
 
-known_image = face_recognition.load_image_file('faces/simon.jpg')
-known_encodings = face_recognition.face_encodings(known_image)
+def find_images_by_person(path, supported_formats=['jpg', 'jpeg', 'png']):
+    """Walks through sub-folders at the given path and finds images in each
+    sub-folder with the given supported formats. The folder structure is
+    assumed to be such that each sub-folder contains images of one person, and
+    that sub-folder is named after a unique identifier for that person.
 
-unknown_image = face_recognition.load_image_file('faces/unknown.jpg')
-unknown_encodings = face_recognition.face_encodings(unknown_image)
+    Example:
+        If the path is 'faces/' the folder-structure should be as follows:
+        faces/
+            john/1.jpg
+            john/2.png
+            doe/1.png
+            doe/3.jpeg
 
-if len(known_encodings) == 0:
-    print("Couldn't find a face in the known image")
-    sys.exit()
+        This folder structure will give a result as follows:
+            {
+                'john': ['faces/john/1.jpg', 'faces/john/2.png'],
+                'doe': ['faces/doe/1.png', 'faces/doe/3.jpeg']
+            }
 
-if len(unknown_encodings) == 0:
-    print("Couldn't find a face in the unknown image")
-    sys.exit()
+    Params:
+        path: The base path to start the search for images from.
+        supported_formats: A list of supported file formats that should be
+            returned in the final list of files.
 
-for unknown_encoding in unknown_encodings:
-    result = face_recognition.compare_faces(known_encodings, unknown_encoding)
-    print(result)
+    Returns:
+        A dictionary with keys taken from the sub-folders names in path, and
+        where each value is a list of paths to each image for that person. The
+        resulting image paths are relative to the given path parameter.
+    """
+    result = collections.defaultdict(list)
+    for (current_dir, dir_names, file_names) in os.walk(path):
+        # Construct a list of filepaths of the files in the current directory.
+        # The filepaths will have the same base as the path parameter.
+        # Furthermore only files with a supported format will be in the list.
+        image_files = [
+            current_dir + "/" + f for f in file_names
+                if f.endswith(tuple(supported_formats))
+        ]
+
+        if not image_files:
+            continue
+
+        # By assumption of the subfolder file structure of the given path,
+        # we find the identifier by taking the name of the current directory.
+        identifier = current_dir.split('/')[-1]
+
+        result[identifier].extend(image_files)
+
+    return result
